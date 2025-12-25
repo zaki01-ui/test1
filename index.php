d9c0c47c9df733e0173747576b91c2a614f9792d
zaki01-ui <ziad.shop.gh@gmail.com>
2025-12-25 09:36:59 +0400

    Add index.php for main page layout

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+$pageTitle = 'ZED_GH | الرئيسية';
+include 'includes/header.php';
+include 'includes/sidebar.php';
+include 'includes/contact-modal.php';
+?>
+
+<div class="main-container">
+    <main class="content">
+        <div class="welcome-banner">
+            <img src="assets/logo.svg" alt="ZED_GH Logo" width="150">
+            <h1 data-i18n="welcome_title">مرحباً بكم في ZED_GH</h1>
+            <p data-i18n="welcome_desc">
+                نسوق للفخامة والاحترافية
+            </p>
+        </div>
+
+        <div id="posts-container" class="products-grid"></div>
+    </main>
+</div>
+
+<?php include 'includes/footer.php'; ?>
