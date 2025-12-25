098a0b9d020c6309f8d90feea8bc9212d9902748
zaki01-ui <ziad.shop.gh@gmail.com>
2025-12-25 09:35:26 +0400

    Add sidebar HTML structure

---
+++ includes/sidebar.php
@@ -0,0 +1,6 @@
+<div id="sidebar-overlay" class="overlay" onclick="toggleSidebar()"></div>
+
+<aside id="sidebar" class="sidebar">
+    <h3 data-i18n="categories">الفئات</h3>
+    <ul class="category-list"></ul>
+</aside>
