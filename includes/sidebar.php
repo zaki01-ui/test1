abc1774da350b0b1fff084e3db8e47c26e060577
zaki01-ui <ziad.shop.gh@gmail.com>
2025-12-25 19:21:15 +0400

    Delete includes directory

--- includes/sidebar.php
+++
@@ -1,6 +0,0 @@
-<div id="sidebar-overlay" class="overlay" onclick="toggleSidebar()"></div>
-
-<aside id="sidebar" class="sidebar">
-    <h3 data-i18n="categories">الفئات</h3>
-    <ul class="category-list"></ul>
-</aside>
