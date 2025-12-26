58475723c01d2c9ac4dfb0c83e2bfb00b280a837
zaki01-ui <ziad.shop.gh@gmail.com>
2025-12-26 19:41:18 +0400

    Create download-links.js

---
+++ js/data/download-links.js
@@ -0,0 +1,13 @@
+// js/data/download-links.js
+const downloadLinks = {
+    android: {
+        local: "https://example.com/android.apk",
+        google_play: "https://play.google.com/store/apps/details?id=xxx",
+        amazon: "https://www.amazon.com/dp/xxx",
+        fdroid: "https://f-droid.org/packages/xxx"
+    },
+    ios: {
+        local: "https://example.com/ios.ipa",
+        app_store: "https://apps.apple.com/app/idxxx"
+    }
+};
