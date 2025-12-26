5d6a922278f0a149394eeb1570bc70b9a039f28b
zaki01-ui <ziad.shop.gh@gmail.com>
2025-12-26 17:45:42 +0400

    Create contact-modal-fad.js

---
+++ js/ui/contact-modal-fad.js
@@ -0,0 +1,7 @@
+function openContactModal() {
+    document.getElementById('contact-modal').classList.add('active');
+}
+
+function closeContactModal() {
+    document.getElementById('contact-modal').classList.remove('active');
+}
