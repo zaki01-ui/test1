0322be488bb0f054bf31614735562ad6608d52d5
zaki01-ui <ziad.shop.gh@gmail.com>
2025-12-28 22:38:18 +0400

    Update supabase-client.js

--- js/data/supabase-client.js
+++ js/data/supabase-client.js
@@ -1,6 +1,6 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
 export const supabase = createClient(
-  "YOUR_PROJECT_URL",
-  "YOUR_ANON_PUBLIC_KEY"
+  "https://bnsgbintiwdnzuqcmfnm.supabase.co",
+  "sb_publishable_uhTFlF4qcTOQ44yzijr71w_lcA-XZop"
 );
